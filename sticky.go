2d5609b09a14d30eea6c323ef7e29d0d18a9007e
Patrik Gaicean <patrik@nassautech.xyz>
2024-10-27 13:14:14 +0200

    move notes logic to switch statement

--- sticky.go
+++ sticky.go
@@ -20,14 +20,10 @@ func main() {
 	f := new(flags)
 
 	flag.StringVar(&f.add, "add", "", "add note")
-	flag.IntVar(&f.del, "del", -1, "delete note by index")
-	flag.IntVar(&f.list, "list", -1, "list one note by index, list all notes for index=0")
-	flag.BoolVar(&f.purge, "purge", false, "purge database for debugging")
+	flag.IntVar(&f.del, "del", 0, "delete note by id")
+	flag.IntVar(&f.list, "list", 0, "list one note by id, list all notes for id=0")
+	flag.BoolVar(&f.purge, "purge", false, "delete all notes")
 	flag.Parse()
-
-	if f.purge {
-		os.Remove("./sticky.db")
-	}
 
 	db, err := sql.Open("sqlite3", "./sticky.db")
 	if err != nil {
@@ -47,50 +43,8 @@ func main() {
 		return
 	}
 
-	count := 0
-	if f.add != "" {
-		count++
-	}
-	if f.del > 0 {
-		count++
-	}
-	if f.list >= 0 {
-		count++
-	}
-
-	if count > 1 {
-		fmt.Println("Error: only one flag is allowed at a time.")
-		flag.Usage()
-		os.Exit(1)
-	}
-
-	if len(f.add) > 0 {
-		stmt, err := db.Prepare("INSERT INTO notes(id, note) values(NULL, ?)")
-		if err != nil {
-			log.Fatal(err)
-		}
-		defer stmt.Close()
-
-		_, err = stmt.Exec(f.add)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		fmt.Println("Successfully added note")
-	} else if f.del > 0 {
-		stmt, err := db.Prepare("DELETE FROM notes WHERE id = ?")
-		if err != nil {
-			log.Fatal(err)
-		}
-		defer stmt.Close()
-
-		_, err = stmt.Exec(f.del)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		fmt.Println("Successfully deleted note #", f.del)
-	} else if f.list > 0 {
+	switch {
+	case f.list != 0:
 		stmt, err := db.Prepare("SELECT id, note FROM notes WHERE id = ?")
 		if err != nil {
 			log.Fatal(err)
@@ -104,7 +58,35 @@ func main() {
 			log.Fatal(err)
 		}
 		fmt.Println(id, note)
-	} else {
+	case f.add != "":
+		stmt, err := db.Prepare("INSERT INTO notes(id, note) values(NULL, ?)")
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer stmt.Close()
+
+		_, err = stmt.Exec(f.add)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Println("Successfully added note")
+	case f.del != 0:
+		stmt, err := db.Prepare("DELETE FROM notes WHERE id = ?")
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer stmt.Close()
+
+		_, err = stmt.Exec(f.del)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Println("Successfully deleted note #", f.del)
+	case f.purge != false:
+		os.Remove("./sticky.db")
+	default:
 		stmt, err := db.Prepare("SELECT id, note FROM notes")
 		if err != nil {
 			log.Fatal(err)
